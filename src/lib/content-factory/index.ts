d62ec66d99b2b98ca7cbf8ec6201f75baff332ba
eliastmax <elias.bsns1@gmail.com>
2026-08-20 16:30:21 +0200

    export Semantic Draft Builder

--- src/lib/content-factory/index.ts
+++ src/lib/content-factory/index.ts
@@ -8,5 +8,6 @@ export * from "./generation-plan";
 export * from "./package-builder";
 export * from "./question-quality";
 export * from "./reports";
+export * from "./semantic-draft";
 export * from "./types";
 export * from "./validators";
